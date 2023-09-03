7c9dc30a247cbf630215c6f3aeef754be9ef7753
amiraallagui <allagui_fawzi@outlook.fr>
2023-09-03 19:27:15 +0100

    view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path: '', redirectTo: 'contacts/admin',  pathMatch: 'full'},
   {path: 'contacts/admin',  component: ContactManagerComponent},
   {path: 'contacts/add',  component: AddContactComponent},
-  {path: 'contacts/edit',  component: EditContactComponent},
+  {path: 'contacts/edit/:contactId',  component: EditContactComponent},
   {path: 'contacts/view/:contactId',  component: ViewContactComponent},
   {path: '**',  component: PageNotFoundComponent},
 ];
