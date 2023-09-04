b9c312ac7acbfe02396748f08c3ff148ee0d158c
amiraallagui <allagui_fawzi@outlook.fr>
2023-09-04 11:33:49 +0100

    contacts

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -12,10 +12,36 @@ export class ContactService {
   
   constructor(private httpClient:HttpClient) { }
 
+// GET ALL contact
   public getAllContacts():Observable<IContact[]> {
     let dataUrl:string =`${ContactService.serverUrl}/contacts`;
     return this.httpClient.get<IContact[]>(dataUrl).pipe(catchError(this.handleError))
   }
+// GET Single contact
+  public getContact(contactId:string):Observable<IContact> {
+    let dataUrl:string =`${ContactService.serverUrl}/contacts/${contactId}`;
+    return this.httpClient.get<IContact>(dataUrl).pipe(catchError(this.handleError))
+  }
+
+  // Create a Contact
+  public createContact(contact:IContact):Observable<IContact>{
+    let dataUrl:string =`${ContactService.serverUrl}/contacts`;
+    return this.httpClient.post<IContact>(dataUrl,contact).pipe(catchError(this.handleError))
+  }
+
+  // Update a Contact
+  public updateSContact(contact:IContact,contactId:string):Observable<IContact>{
+    let dataUrl:string =`${ContactService.serverUrl}/contacts/${contactId}`;
+    return this.httpClient.put<IContact>(dataUrl,contact).pipe(catchError(this.handleError))
+  }
+
+  // Delete a Contact
+  public deleteSContact(contactId:string):Observable<{}>{
+    let dataUrl:string =`${ContactService.serverUrl}/contacts/${contactId}`;
+    return this.httpClient.delete<{}>(dataUrl).pipe(catchError(this.handleError))
+  }
+
+  // Get All Groups
 
   public handleError(error:HttpErrorResponse){
     let errorMsg:string = '';
