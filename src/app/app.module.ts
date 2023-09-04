cb52a740e94f292150c5f952b556bca5173fc871
amiraallagui <allagui_fawzi@outlook.fr>
2023-09-04 10:56:59 +0100

    handle error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { EditContactComponent } from './components/edit-contact/edit-contact.com
 import { ViewContactComponent } from './components/view-contact/view-contact.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { IContact } from '../models/IContact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  
+  private static serverUrl: string = 'http://localhost:9000';
+  
+  constructor(private httpClient:HttpClient) { }
 
-  constructor() { }
+  public getAllContacts():Observable<IContact[]> {
+    let dataUrl:string =`${ContactService.serverUrl}/contacts`;
+    return this.httpClient.get<IContact[]>(dataUrl).pipe(catchError(this.handleError))
+  }
+
+  public handleError(error:HttpErrorResponse){
+    let errorMsg:string = '';
+    if(error.error instanceof ErrorEvent){
+      // client error
+      errorMsg =`Error: ${error.error.message}`
+    }
+    else {
+     // server error
+      errorMsg =`Status: ${error.status}\n Message:${error.message}`
+    }
+    return throwError(errorMsg)
+  }
 }
